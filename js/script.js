5616aef9e75a028aad4ccd733d8e3078d52fc7dd
Manash <manashmndl@gmail.com>
2019-03-01 02:05:55 +0600

    fixed all issues thanks to graphlib

--- js/script.js
+++ js/script.js
@@ -67,10 +67,29 @@ function expressionParser(text){
 
     // variable transaction group
     let _variableTransactionGroup = _.zip(allVariables, allTransactions).map(x => {return {variable: x[0], transaction: x[1]}}).groupBy('transaction')
+    let _variableToTransactionMap = _.zip(allVariables, allTransactions).map(x => {return {variable: x[0], transaction: x[1]}}).groupBy('variable')
+
     let variableTransactionGroup = {}
+    let variableToTransactionMap = {}
+
     Object.keys(_variableTransactionGroup).map((k, i) => {
         variableTransactionGroup[k] = Array.from(new Set(_variableTransactionGroup[k].map(x => x.variable)))
     })
+
+    Object.keys(_variableToTransactionMap).map((k, i) => {
+        variableToTransactionMap[k] = _variableToTransactionMap[k].map(x => x.transaction).reduce((acc, value) => {
+
+            if (acc.length === 0){
+                acc.push(value)
+            } else if ( acc[acc.length - 1] !== value ){
+                acc.push(value)
+            }
+            return acc;
+    
+        }, []);
+    })
+
+    console.log({var: variableToTransactionMap})
 
     _.each(transactions, (T)=>{
         $("#transactions").append(
@@ -91,18 +110,25 @@ function expressionParser(text){
         nodes.push({data: { id: T, name: T }})
     })
 
-    for (let i = 0; i < transactions.length; i++){
-        for (let j = 0; j < transactions.length - 1; j++){
-            if (transactions[i] !== transactions[j + 1]){
-
-                _edges.push({
-                    source: transactions[i],
-                    target: transactions[j + 1]
-                })
-
-            }
+    Object.keys(variableToTransactionMap).map(k => {
+        let trx = variableToTransactionMap[k];
+        for (let i = 0; i < trx.length - 1; i++){
+            _edges.push({source: trx[i], target: trx[i + 1]})
         }
-    }
+    })
+
+    // for (let i = 0; i < transactions.length; i++){
+    //     for (let j = 0; j < transactions.length - 1; j++){
+    //         if (transactions[i] !== transactions[j + 1]){
+
+    //             _edges.push({
+    //                 source: transactions[i],
+    //                 target: transactions[j + 1]
+    //             })
+
+    //         }
+    //     }
+    // }
 
     let edges = _edges.filter(function (a) {
         var key = a.source + '|' + a.target;
@@ -113,21 +139,28 @@ function expressionParser(text){
     }, Object.create(null));
 
 
-    let connectedEdgesByVariable = [];
+    // let connectedEdgesByVariable = [];
 
-    for (let j = 0; j < edges.length; j++){
-        let sourceVariables = variableTransactionGroup[ edges[j].source ];
-        let targetVariables = variableTransactionGroup[ edges[j].target ];
+    // for (let j = 0; j < edges.length; j++){
+    //     let sourceVariables = variableTransactionGroup[ edges[j].source ];
+    //     let targetVariables = variableTransactionGroup[ edges[j].target ];
 
-        let commonVariables = _.intersection(sourceVariables, targetVariables);
+    //     let commonVariables = _.intersection(sourceVariables, targetVariables);
 
-        if (commonVariables.length > 0){
-            connectedEdgesByVariable.push( edges[j] )
-        }
-    }
+    //     if (commonVariables.length > 0){
+    //         connectedEdgesByVariable.push( edges[j] )
+    //     }
+    // }
 
+    // For graphlib
+    let G = new graphlib.Graph();
+    Array.from(new Set(allTransactions)).map(T => {
+        G.setNode(T)
+    })
 
-    let reducedEdges = connectedEdgesByVariable.map(function(d){
+    let reducedEdges = edges.map(function(d){
+        G.setEdge(d.source, d.target);
+
         return {
             data: {
                 source: d.source,
@@ -163,7 +196,7 @@ function expressionParser(text){
         ]
     })
 
-    let verdict = cy.edges().isSimple();
+    let verdict = !graphlib.alg.isAcyclic(G); //cy.edges().isLoop();
 
     // console.log(cy.)
     $("#verdict").attr('class', conflictMessageObject[verdict].alert).text(conflictMessageObject[verdict].message)
