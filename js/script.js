1c861d4322a9d8cd4073db8a76e17e69573714c4
Manash <manashmndl@gmail.com>
2019-02-28 20:55:06 +0600

    fixed all

--- js/script.js
+++ js/script.js
@@ -23,10 +23,22 @@ function clearAll(){
 
 function expressionParser(text){
     if (text !== undefined){
+
+    text = text.trim();
     
     clearAll(); 
         // Replace operations with transactions 
-    let transactions = Object.keys(_.groupBy(text.replace(new RegExp('w|r', 'g'), 'T').replace(/\(.\w*\)/gi, '').split(' ')))
+    // let transactions = Object.keys(_.groupBy(text.replace(new RegExp('w|r', 'g'), 'T').replace(/\(.\w*\)/gi, '').split(' ')))
+    let transactions = text.replace(new RegExp('w|r', 'g'), 'T').replace(/\(.\w*\)/gi, '').split(' ').reduce( (acc, value) => {
+        
+        if (acc.length === 0){
+            acc.push(value)
+        } else if ( acc[acc.length - 1] !== value ){
+            acc.push(value)
+        }
+        return acc;
+
+    }, []);
 
     let variables = Array.from(new Set(text.match(/\(.\w*\)/gi).map(v => v.replace('(', '').replace(')', ''))))
     
@@ -45,6 +57,58 @@ function expressionParser(text){
             `<span class="badge badge-secondary">${V}</span>`
         )
     })
+
+    let nodes = [];
+    let _edges = [];
+
+    _.each(Array.from(new Set(transactions)), (T) => {
+        nodes.push({data: { id: T, name: T }})
+    })
+
+    for (let i = 0; i < transactions.length - 1; i++){
+        _edges.push({source: transactions[i],
+                    target: transactions[i + 1]
+        })
+    }
+
+    let edges = _edges.filter(function (a) {
+        var key = a.source + '|' + a.target;
+        if (!this[key]) {
+            this[key] = true;
+            return true;
+        }
+    }, Object.create(null));
+
+    let reducedEdges = edges.map(function(d){
+        return {
+            data: {
+                source: d.source,
+                target: d.target
+            }
+        }
+
+    });
+
+    cytoscape({
+
+        container: document.getElementById('viz'), // container to render in
+      
+        elements: {
+            nodes: nodes,
+            edges: reducedEdges
+        },
+
+        style: [
+            {
+                selector: 'edge',
+                style: {
+                  'curve-style': 'bezier',
+                  'target-arrow-shape': 'triangle'
+                }
+            },
+        ]
+    })
+    
 
     }
     
@@ -67,15 +131,58 @@ var edges = new vis.DataSet([
     {from: 2, to: 5}
 ]);
 
-// create a network
-var container = document.getElementById('viz');
+// // create a network
+// var container = document.getElementById('viz');
 
-// provide the data in the vis format
-var data = {
-    nodes: nodes,
-    edges: edges
-};
-var options = {};
+// // provide the data in the vis format
+// var data = {
+//     nodes: nodes,
+//     edges: edges
+// };
+// var options = {};
 
-// initialize your network!
-var network = new vis.Network(container, data, options);
+// // initialize your network!
+// var network = new vis.Network(container, data, options);
+
+// var cy = cytoscape({
+
+//     container: document.getElementById('viz'), // container to render in
+  
+//     elements: [ // list of graph elements to start with
+//       { // node a
+//         data: { id: 'a' }
+//       },
+//       { // node b
+//         data: { id: 'b' }
+//       },
+//       { // edge ab
+//         data: { id: 'ab', source: 'a', target: 'b' }
+//       }
+//     ],
+  
+//     style: [ // the stylesheet for the graph
+//       {
+//         selector: 'node',
+//         style: {
+//           'background-color': '#666',
+//           'label': 'data(id)'
+//         }
+//       },
+  
+//       {
+//         selector: 'edge',
+//         style: {
+//           'width': 3,
+//           'line-color': '#ccc',
+//           'target-arrow-color': '#ccc',
+//           'target-arrow-shape': 'triangle'
+//         }
+//       }
+//     ],
+  
+//     layout: {
+//       name: 'grid',
+//       rows: 1
+//     }
+  
+//   });
